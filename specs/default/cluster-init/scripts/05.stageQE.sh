9c25cffa807eb663de6db4bb60f358985871d2c2
Jer-Ming Chia <jer-ming.chia@cyclecomputing.com>
2017-11-10 09:30:29 -0500

    Unpack files only if they exist

--- specs/default/cluster-init/scripts/05.stageQE.sh
+++ specs/default/cluster-init/scripts/05.stageQE.sh
@@ -1,3 +1,5 @@
 #! /bin/bash -e
 
-tar xf $CYCLECLOUD_SPEC_PATH/files/espresso-5.4.0.centos71.intel.tgz -C /mnt/resource 
+if [ test -f $CYCLECLOUD_SPEC_PATH/files/espresso-5.4.0.centos71.intel.tgz ];then
+    tar xf $CYCLECLOUD_SPEC_PATH/files/espresso-5.4.0.centos71.intel.tgz -C /mnt/resource 
+fi
